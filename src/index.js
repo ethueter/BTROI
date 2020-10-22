9ed10c048f8f2198cdb71e8a63d11bf0aaa2dd21
Tyler Hueter <tylerhueter08@gmail.com>
2020-10-21 22:31:36 -0400

    setting src and initial index

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Home = () => {
+    
+    return (
+        <div>
+            <h1>Begining of Tech Assessment</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<Home />, document.getElementById("root"));
